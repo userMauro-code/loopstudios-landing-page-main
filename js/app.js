550ff583a9793713ab594ede89f21918e9ed5976
userMauro-code <m88642336@gmail.com>
2025-11-13 20:26:38 -0300

    diseño mobile terminado  y agregado de menu desplegable

--- js/app.js
+++ js/app.js
@@ -6,10 +6,14 @@ btnMenu.addEventListener("click", ()=>{
     if(btnMenu.getAttribute("aria-expanded") === "false"){
         btnMenu.setAttribute("aria-expanded", "true");
         menu.hidden = false;
-        btnClose.setAttribute("src", "../images/icon-close.svg")
+        btnClose.setAttribute("src", "../images/icon-close.svg");
+        document.body.style.overflow = "hidden";
+        menu.classList.add("header__nav-active");
     }else{
         btnMenu.setAttribute("aria-expanded", "false");
         menu.hidden = true;
         btnClose.setAttribute("src", "../images/icon-hamburger.svg");
+        document.body.style.overflow = "auto";
+        menu.classList.remove("header__nav-active");
     }
 });
